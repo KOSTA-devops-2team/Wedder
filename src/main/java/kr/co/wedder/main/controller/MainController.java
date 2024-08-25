0c283763bc7f3aa4f037b26f057150c610f7450a
cherryJubilee <hyewon5026@gmail.com>
2024-08-26 02:18:39 +0900

    feat : [BE] 메인, 어드민 페이지 JSP로 변경

---
+++ src/main/java/kr/co/wedder/main/controller/MainController.java
@@ -0,0 +1,16 @@
+package kr.co.wedder.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/common/footer.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hyewon
+  Date: 8/26/24
+  Time: 1:15 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
