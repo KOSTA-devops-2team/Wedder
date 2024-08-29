6e5c059c937b410c358a5d0105986407d2d93077
JeongHwa518 <jeonghwa485@gmail.com>
2024-08-29 16:40:58 +0900

    Remove target folder from tracking

--- target/wedder-1.0.0-BUILD-SNAPSHOT/resources/js/mypage/coupling.js
+++
@@ -1,6 +0,0 @@
-const modal = document.querySelector('.coupling-modal');
-const btnOpenModal=document.querySelector('.open-modal');
-
-btnOpenModal.addEventListener("click", ()=>{
-    modal.style.display="block";
-});
