d1976290b183db012ff3dffa88d8f62b36e7bfb8
cherryJubilee <hyewon5026@gmail.com>
2024-08-29 22:33:31 +0900

    feat : [BE] URL 패턴 소문자로 통일

---
+++ src/main/java/kr/co/wedder/sdmpackage/controller/packageController.java
@@ -0,0 +1,26 @@
+package kr.co.wedder.sdmpackage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/package")
+public class packageController {
+
+    @GetMapping(value = "")
+    public String packageMain() {
+        return "package/packageMain";
+    }
+
+    @GetMapping(value = "/detail")
+    public String packageDetail() {
+        return "package/packageDetail";
+    }
+
+
+    @GetMapping(value = "/recommend")
+    public String packageRecommend() {
+        return "package/packageRecommend";
+    }
+}
