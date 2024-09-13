24971039f5c0eaae9c91a557f3a8fc4f4c92daa8
JeongHwa518 <jeonghwa485@gmail.com>
2024-09-13 11:30:30 +0900

    feat : [BE] hallList page

---
+++ src/main/java/kr/co/wedder/company/dao/CompanyDao.java
@@ -0,0 +1,21 @@
+package kr.co.wedder.company.dao;
+
+import kr.co.wedder.company.domain.CompanyDto;
+import kr.co.wedder.company.service.SearchItem;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CompanyDao {
+
+    CompanyDto selectCompanyById(Integer companyId) throws Exception;
+    List<CompanyDto> selectCompanyByCategory() throws Exception;
+
+    List<CompanyDto> selectPage(Map map) throws Exception;
+    List<CompanyDto> searchSelectPage(SearchItem sc) throws Exception;
+
+    int count() throws Exception;
+    int searchResultCnt(SearchItem sc) throws Exception;
+
+
+}
---
+++ src/main/java/kr/co/wedder/company/service/CompanyServiceImpl.java
@@ -0,0 +1,49 @@
+package kr.co.wedder.company.service;
+
+import kr.co.wedder.company.dao.CompanyDao;
+import kr.co.wedder.company.domain.CompanyDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class CompanyServiceImpl implements CompanyService {
+
+    @Autowired
+    CompanyDao companyDao;
+
+    @Override
+    public List<CompanyDto> getPage(Map map) throws Exception {
+        return companyDao.selectPage(map);
+    }
+
+    @Override
+    public int getCount() throws Exception {
+        return companyDao.count();
+    }
+
+    @Override
+    public List<CompanyDto> getSearchResultPage(SearchItem sc) throws Exception {
+        return companyDao.searchSelectPage(sc);
+    }
+
+    @Override
+    public int getSearchResultCount(SearchItem sc) throws Exception {
+        return companyDao.searchResultCnt(sc);
+    }
+
+    @Override
+    public CompanyDto companyRead(Integer companyId) throws Exception {
+        return companyDao.selectCompanyById(companyId);
+    }
+
+    @Override
+    public List<CompanyDto> getHallList() throws Exception {
+        List<CompanyDto> companyList = companyDao.selectCompanyByCategory();
+        System.out.println("Service : getHallList");
+        return companyList;
+    }
+
+}
