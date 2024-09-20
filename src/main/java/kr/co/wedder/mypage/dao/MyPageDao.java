3eed96cf7774acd1277647072e816af745350bd6
jongwon <127845759+parkblacksheep@users.noreply.github.com>
2024-09-20 17:59:47 +0900

    chore : [BE] git merging

---
+++ src/main/java/kr/co/wedder/mypage/dao/MyPageDao.java
@@ -0,0 +1,35 @@
+package kr.co.wedder.mypage.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import kr.co.wedder.company.domain.CompanyDto;
+import kr.co.wedder.mypage.domain.HistoryDto;
+import kr.co.wedder.mypage.domain.MyPageDTO;
+import kr.co.wedder.mypage.domain.ReservationDto;
+import kr.co.wedder.mypage.domain.VisitCriteria;
+
+public interface MyPageDao {
+	
+	//mypage
+	MyPageDTO selectOne(Integer customer_id) throws Exception;
+	
+	//company
+	CompanyDto selectCompany(Integer company_id) throws Exception;
+	
+	//reservation
+	ReservationDto selectReservation(Integer reservation_id) throws Exception;
+	
+	//History
+	HistoryDto selectHistory(Integer customer_id) throws Exception;
+	
+	//금일 방문 일정 카운트 가져오기 
+	Integer todayVisitCount(Map<String,Object> map) throws Exception;
+	
+	//join 금일 방문 일정 
+	List<VisitCriteria> todayVisitHistory(Map<String, Object> map) throws Exception;
+	
+	//company list를 가져오기 위해서 
+	List<CompanyDto> todayReservationHistory(Map<String, Object> map) throws Exception; 
+	
+}
